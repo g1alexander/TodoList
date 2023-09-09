e1aa1c1be04b040eec230365ca1d0b2487d883d5
g1alexander <alexander.granados.dev@gmail.com>
2023-09-09 09:33:38 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by g1alexander on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
